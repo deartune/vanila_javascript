03887b53579acc56bed1749f377f5c7a904b96de
deartune <deartune@gmail.com>
2018-12-16 02:17:09 +0900

    object organization

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 ﻿// JavaScript source code
 
-const b="seoHyun";
-const array=["😃","👱","💝",34,true,33.5,b];
+const seoHyunInfo={name:"seoHyun",age:3,isCute:true,city:"sungNam", favorateFood:
+["gim","carrot","jelly"],toys:[
+{name:"kongSunEe",isSheLikes:true},{name:"tiranosaurus",isSheLikes:true},{name:"Koongfoopanther",isSheLikes:false}
+]
+}//object variable
 
-console.log(array);
 
-console.log(array[1]);
+console.log(seoHyunInfo.isCute);
+console.log(seoHyunInfo.city);
+
+
+//const object variable's members can be changed
+seoHyunInfo.city="yongIn";
+console.log(seoHyunInfo.city);
+
+
+console.log(seoHyunInfo.toys[0]);
+console.log(seoHyunInfo.toys[0].isSheLikes);
+console.log(seoHyunInfo.favorateFood[2]);
