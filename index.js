9ea8eb5d94f51ce5fb97df0d41eb802cc7679458
deartune <deartune@gmail.com>
2018-12-16 01:15:36 +0900

    const / let

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // JavaScript source code
-let a=215;
-let b=a+4;
- a=4;
+let c=215;
+let d=c+4;
+ c=4;
+console.log(c,d);
+
+const a=215;
+const b=a+4;
+//constant variable cannot changed
 console.log(b,a);
