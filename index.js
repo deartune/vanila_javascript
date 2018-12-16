268a0e0e0075782b481bcfec0480ba65dc883f6b
deartune <deartune@gmail.com>
2018-12-17 00:10:07 +0900

    getElementById/innerHTML

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 ﻿// JavaScript source code
 
-function monday(time,task){
-return("Monday "+time+" we have to "+task)
-
-};
-
-const onMonday = monday("8:00","go work");
-console.log(onMonday);
-
-monday("8:00","go work");
 
 
+const changeTitle = document.getElementById('title')
+changeTitle.innerHTML="Js"
 
-const calculator={
-plus:function(a,b){return a+b;},
-minus:function(a,b){return a-b},
-divide:function(a,b){return a/b},
-multiple:function(a,b){return a*b}
-}
-
-const plusSample = calculator.plus(4,5);
-console.log(plusSample);
-
-const minusSample =calculator.minus(12,7);
-console.log(minusSample);
-
-const divideSample =calculator.divide(20,2);
-console.log(divideSample);
-
-const multipleSample = calculator.multiple(3,4);
-console.log(multipleSample);
