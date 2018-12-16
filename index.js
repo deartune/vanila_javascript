e6138ee91897ae924bceee6cad4e9d7d3ff87303
deartune <deartune@gmail.com>
2018-12-16 23:05:52 +0900

    function parameter

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 ﻿// JavaScript source code
 
-const seoHyunInfo={name:"seoHyun",age:3,isCute:true,city:"sungNam", favorateFood:
-["gim","carrot","jelly"],toys:[
-{name:"kongSunEe",isSheLikes:true},{name:"tiranosaurus",isSheLikes:true},{name:"Koongfoopanther",isSheLikes:false}
-]
-}//object variable
+function monday(time,task){
+console.log("Monday "+time+" we have to "+task)
+};
+
+monday("8:00","go work");
 
 
-console.log(seoHyunInfo.isCute);
-console.log(seoHyunInfo.city);
 
+function tuesday(time,task){
+console.log("Tuesday ",time," i enjoy ",task)
+};
 
-//const object variable's members can be changed
-seoHyunInfo.city="yongIn";
-console.log(seoHyunInfo.city);
-
-
-console.log(seoHyunInfo.toys[0]);
-console.log(seoHyunInfo.toys[0].isSheLikes);
-console.log(seoHyunInfo.favorateFood[2]);
+tuesday("20:00","drama");
