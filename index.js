7d3f48e04170d35c9f681296ea3cdc1c1e1b3105
deartune <deartune@gmail.com>
2018-12-16 23:26:45 +0900

    javascript string variable declaration ' ${string} '

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ monday("8:00","go work");
 
 
 
-function tuesday(time,task){
-console.log("Tuesday ",time," i enjoy ",task)
+function tuesday(time,something){
+console.log("Tuesday ",time," i enjoy ",something)
 };
 
 tuesday("20:00","drama");
+
+
+function wednesday(time,meal){
+console.log(`Wednesday ${time} i eat ${meal}`)
+};
+
+wednesday("18:00","rice noodle")
