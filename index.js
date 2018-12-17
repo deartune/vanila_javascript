d8f6df4ae2461b9ea3a04bd21fed68b73bd13ecd
deartune <deartune@gmail.com>
2018-12-17 13:52:25 +0900

    if else

--- index.js
+++ index.js
@@ -5,13 +5,37 @@
 const changeTitle = document.querySelector('#title')
 changeTitle.innerHTML="Js"
 changeTitle.style.color="blue";
-document.title="time goes on"
+document.title="vanila_javascript"
 //console.log(changeTitle.style);
 
-
+/*
 window.addEventListener("resize",handleResize)
-
-
 function handleResize(){
-    console.log("Your browse resized")
+    console.log("Your browser resized")
+}
+*/
+
+
+window.addEventListener("resize",eventReport)
+function eventReport(event){
+    console.log(event)
+}
+window.addEventListener("resize",eventReport)
+
+
+changeTitle.addEventListener("click",clickEvent)
+function clickEvent(){
+    changeTitle.style.color="red";
+    
+}
+
+const age =prompt("How old are you?")
+
+if(age<20){
+    console.log("You are teenager")
+}
+else if(20<=age && age<30){
+    console.log("You have beautiful skin")
+}else{
+    console.log("i recommend some exercise like swimming")
 }
