e128c87a5ebe14491854b84896bf9eec44581315
deartune <deartune@gmail.com>
2018-12-17 13:19:46 +0900

    eventHandle

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const changeTitle = document.querySelector('#title')
 changeTitle.innerHTML="Js"
 changeTitle.style.color="blue";
 document.title="time goes on"
+//console.log(changeTitle.style);
+
+
+window.addEventListener("resize",handleResize)
+
+
+function handleResize(){
+    console.log("Your browse resized")
+}
